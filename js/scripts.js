ceec395f42a71c8ad60d62895f5019de847d095c
jd <duverseaujohnny21@gmail.com>
2020-06-12 18:08:10 -0400

    fixing my code

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,29 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
-const userInput = $("input#typeNum").val();
-const str = userInput;
-const string = str.toString();
-const newstr = string.include()
-  console.log(newstr.split(""));
-// $("#outcome").text(newstr.split(" "));
+const userInput = $("input#enterNumber").val();
+ const string = global(userInput);
+ $("#outCome").text(string);
+  
 
   });
 });
 
 //Business logic
 function global(number) {
-  let array = [];
-  for (i = 0;  i < number.length; i++) {
-    if (arrays)
+  const array = [];
+  for (index = 0;  index <= number; index++) {
+    array.push(index.toString());
+    if((array[index]).includes("1")) {
+      array[index] = ("Beep");
+    } else if ((array[index]).includes("2")) {
+      array[index] = ("Boop");
+    } else if ((array[index]).includes("3")) {
+      array[index] = ("Won't you be my neighbor");
+    } else {
+      array[index];
+    }
   }
+  return array;
 }
   

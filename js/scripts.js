9a30245d13a0c5c6cd17ce5de10df1c670175392
jd <duverseaujohnny21@gmail.com>
2020-06-12 15:15:43 -0400

    add jquery business logic

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+const userInput = $("input#typeNum").val();
+const str = userInput;
+const string = str.toString();
+const newstr = string.include()
+  console.log(newstr.split(""));
+// $("#outcome").text(newstr.split(" "));
+
+  });
+});
+
+//Business logic
+function global(number) {
+  let array = [];
+  for (i = 0;  i < number.length; i++) {
+    if (arrays)
+  }
+}
+  
